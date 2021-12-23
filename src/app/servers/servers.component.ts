d19f1ce9e61564e508f85bf2b9b1a62f8e777ecd
cornellreddick <cornellreddick@outlook.com>
2021-12-23 13:10:18 -0500

    Create a username input field that can be reset.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,53 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  allowUserName = false;
+  serverCreationStatus = "No server was created!";
+  resetUserName = '';
+  serverName = 'Testserver';
+  userName = '';
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+
+    if (!this.isEmptyOrSpaces(this.userName)){
+      this.allowUserName = true;
+    } 
+
+  }
 
   ngOnInit(): void {
   }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onReset(){
+    this.userName = '';
+    
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
+  }
+
+  onUpdateUserName(event: any){
+    this.userName = event.target.value; 
+  }
+
+  isEmptyOrSpaces(str){
+    return str === null || str.match(/^ *$/) !== null;
+}
 
 }
