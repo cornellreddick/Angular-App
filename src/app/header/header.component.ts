13bd8b0a6e592f842418d7625c0c23bf8d207745
Cornell Reddick <cornellreddick@outlook.com>
2022-01-31 17:52:28 -0500

    update the navigation so that Recipes and Shopping list are displayed separately

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
   collapsed = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
+  }
+
 
 }
