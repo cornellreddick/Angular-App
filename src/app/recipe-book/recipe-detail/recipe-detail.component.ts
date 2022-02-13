6e2e424711887752abc3ea4cde1e4423b2cab438
Cornell Reddick <cornellreddick@outlook.com>
2022-02-13 15:45:13 -0500

    Fixed a bug in recipe-detail.component and updated the styling for the html.

--- src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Output() recipe: Recipe; 
+  @Input() recipe: Recipe; 
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -11,8 +11,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://stateofdinner.com/wp-content/uploads/2021/04/Pasta_With_Ricotta_Cooked_Overhead.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://stateofdinner.com/wp-content/uploads/2021/04/Pasta_With_Ricotta_Cooked_Overhead.jpg')
+    new Recipe('First Recipe', 'This is simply a test', 'https://stateofdinner.com/wp-content/uploads/2021/04/Pasta_With_Ricotta_Cooked_Overhead.jpg'),
+    new Recipe('Second Recipe', 'This is simply a test', 'https://stateofdinner.com/wp-content/uploads/2021/04/Pasta_With_Ricotta_Cooked_Overhead.jpg')
   ];
 
   constructor() { }
